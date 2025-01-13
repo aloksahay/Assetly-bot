a6492e2472d165541bf0c016d0ccbdbc0104ee23
Alok Sahay <alok.sahay87@gmail.com>
2025-01-13 02:04:49 +0100

    Updated Next Config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname),
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@hooks': path.resolve(__dirname, 'hooks'),
     };
     return config;
   }
