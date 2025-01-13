7017639434477479d952a8b9980a65a3c7f6a15e
Alok Sahay <alok.sahay87@gmail.com>
2025-01-13 02:32:15 +0100

    Removed Next files from commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-      '@lib': path.resolve(__dirname, 'lib'),
-      '@utils': path.resolve(__dirname, 'utils'),
-      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@': '.',
     };
     return config;
   }
