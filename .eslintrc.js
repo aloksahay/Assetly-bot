fe8c49000c45d33f7ac2915adab82f9ed9a74414
Alok Sahay <alok.sahay87@gmail.com>
2025-01-13 06:18:50 +0100

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,8 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next/core-web-vitals'
-  ],
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
+  extends: ['next'],
   settings: {
-    react: {
-      version: 'detect'
+    next: {
+      rootDir: ['./']
     }
-  },
-  rules: {
-    // Add any specific rules here
   }
 }; 
